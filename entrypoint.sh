e1d67c619dea22f18e7c991af22fff72d4972395
Augusto Andrade <alias.tracking.na@gmail.com>
2026-02-28 19:12:48 -0400

    Fix volume mounting and gunicorn command in Docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,4 @@ echo "Collecting static files..."
 python manage.py collectstatic --noinput --clear
 
 echo "Starting Gunicorn..."
-exec gunicorn --bind :8000 --workers 1 --worker-class sync --worker-tmp-dir /dev/shm --timeout 30 --graceful-timeout 5 --access-logfile - --error-logfile - sisnum.wsgi:application
+exec python -m gunicorn --bind :8000 --workers 1 --worker-class sync --worker-tmp-dir /dev/shm --timeout 30 --graceful-timeout 5 --access-logfile - --error-logfile - sisnum.wsgi:application
