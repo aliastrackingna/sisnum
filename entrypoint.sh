0be65d0ce230a554635e91f9a0a345803e5f593f
Augusto Andrade <alias.tracking.na@gmail.com>
2026-02-28 18:45:27 -0400

    Reduce Gunicorn workers to 1 for container

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,4 @@ echo "Collecting static files..."
 python manage.py collectstatic --noinput --clear
 
 echo "Starting Gunicorn..."
-exec gunicorn --bind :8000 --workers 3 --worker-class sync --worker-tmp-dir /dev/shm --timeout 30 --graceful-timeout 5 --access-logfile - --error-logfile - sisnum.wsgi:application
+exec gunicorn --bind :8000 --workers 1 --worker-class sync --worker-tmp-dir /dev/shm --timeout 30 --graceful-timeout 5 --access-logfile - --error-logfile - sisnum.wsgi:application
